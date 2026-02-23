641d82575166510f3b0390747f1594435de0fa3d
zia-aamir <aamirshanediwan@gmail.com>
2026-02-23 23:19:39 +0530

    its 11:19 PM now

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,8 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
+echo "i love my india"
+echo "this is pune"
+echo "this is new india"
+
 
